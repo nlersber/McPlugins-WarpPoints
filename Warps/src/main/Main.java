66f4f4f635f1b876d6780c6ce26d6f8acf592bb0
Nick Lersberghe <nick.lersberghe@student.hogent.be>
2019-01-28 18:22:00 +0100

    Added TabCompleters, removed empty lines from plugin.yml

--- Warps/src/main/Main.java
+++ Warps/src/main/Main.java
@@ -10,6 +10,7 @@ import commands.WarpExecutor;
 import commands.WarpsExecutor;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
@@ -19,6 +20,7 @@ import listeners.JoinListener;
 import org.bukkit.entity.Player;
 import org.bukkit.permissions.PermissionAttachment;
 import org.bukkit.plugin.java.JavaPlugin;
+import tabCompleters.WarpTabCompleter;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -30,41 +32,76 @@ import org.bukkit.plugin.java.JavaPlugin;
  * @author Nick
  */
 public class Main extends JavaPlugin {
-    
+
     private DataManager manager;
-    
+
     @Override
     public void onEnable() {
-        
+
         File dataFolder = getDataFolder();//Checks if datafolder already exists and makes it if it doesn't
         if (!dataFolder.exists())
             dataFolder.mkdir();
-        
+
         loadConfig();
-        
+
         List<Player> players = new ArrayList<>();
-        
+
         players.addAll(this.getServer().getOnlinePlayers());//Get list of all online players
 
         manager = new DataManager(this);//Initiates the DataManager
 
         Stream<UUID> temp = players.stream().map(s -> s.getUniqueId());
-        
+
         manager.fillUpFromFiles(temp == null ? new ArrayList<>() : temp.filter(s -> s != null)
                 .collect(Collectors.toList()));//Fills up the Data if the playerList isn't empty to avoid nullpointers
 
         new JoinListener(this);
-        
+
         this.getCommand("setwarp").setExecutor(new SetWarpExecutor(this));
-        this.getCommand("rmwarp").setExecutor(new RmWarpExecutor(this));
-        this.getCommand("chwarp").setExecutor(new ChWarpExecutor(this));
-        this.getCommand("warps").setExecutor(new WarpsExecutor(this));
+
+        this.getCommand("removewarp").setAliases(Arrays.asList("rmwarp", "remwarp", "rwarp", "rmvwarp"));
+        this.getCommand("removewarp").setExecutor(new RmWarpExecutor(this));
+        this.getCommand("removewarp").setTabCompleter(new WarpTabCompleter(this));
+
+        this.getCommand("changewarp").setAliases(Arrays.asList("chwarp", "changewarpname", "cwarp"));
+        this.getCommand("changewarp").setExecutor(new ChWarpExecutor(this));
+        this.getCommand("changewarp").setTabCompleter(new WarpTabCompleter(this));
+
+        this.getCommand("warplist").setAliases(Arrays.asList("warps", "listwarp", "getwarps"));
+        this.getCommand("warplist").setExecutor(new WarpsExecutor(this));
+
         this.getCommand("warp").setExecutor(new WarpExecutor(this));
+        this.getCommand("warp").setTabCompleter(new WarpTabCompleter(this));
+
         this.getCommand("activatewarpworld").setExecutor(new ActivateWarpExecutor(this));
+
         this.getCommand("removewarpworld").setExecutor(new RemoveWarpWorldExecutor(this));
+
         this.getCommand("activatewarps").setExecutor(new ActivateWarpExecutor(this));
+
     }
-    
+
+    /*
+    commands:
+    setwarp:
+        usage: /<command> [warp name]
+        description: Sets a warp point  
+    removewarp:
+        usage: /<command> [warp name]
+        description: Removes a warp point
+        aliases: [rmwarp, remwarp, rwarp, rmvwarp]
+    changewarp:
+        usage: /<command> [current warp name] [new warp name]
+        description: Changes the name of a set warp
+        aliases: [chwarp, changewarpname, cwarp]
+    warplist:
+        usage: /<command>
+        description: Gives a list of all the warps you have set
+        aliases: [warps, listwarp, getwarps]
+    warp:
+        usage: /<command> [warp name]
+        description: Teleports you to the specified warp
+     */
     public void loadConfig() {
         getConfig().options().copyDefaults(true);
         saveConfig();
---
+++ Warps/src/tabCompleters/WarpTabCompleter.java
@@ -0,0 +1,35 @@
+package tabCompleters;
+
+import Utils.DataManager;
+import java.util.Arrays;
+import java.util.List;
+import main.Main;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+
+/**
+ *
+ * @author Nick
+ */
+public class WarpTabCompleter implements TabCompleter {
+
+    private Main plugin;
+
+    public WarpTabCompleter(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender cs, Command cmnd, String string, String[] strings) {
+        if (!(cs instanceof Player)) {
+            cs.sendMessage("You're not a player!");
+            return null;
+        }
+
+        Player p = (Player) cs;
+        return Arrays.asList(DataManager.getWarpNames(p.getUniqueId()).split("\n"));
+    }
+
+}
