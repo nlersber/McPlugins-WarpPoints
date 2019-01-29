05c6b7ea02c9a1a8e5b04904b1595629cee3952e
Nick Lersberghe <nick.lersberghe@student.hogent.be>
2019-01-29 01:14:33 +0100

    Added comments, changed package names to fit convention

---
+++ Warps/src/utils/ChatUtils.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import org.bukkit.ChatColor;
+
+/**
+ *
+ * @author Nick
+ */
+public class ChatUtils {
+
+    public static String chat(String message) {
+        return ChatColor.translateAlternateColorCodes('&', message);//Adds a color to the chat text. Deprecated in this project, use 'ChatColor.ENUMVALUE + "String" instead for cleaner code
+    }
+}
